e24838874d0414d7de081c2724e81104ca2913a9
HARISH A K <116714372+harish-AK@users.noreply.github.com>
2023-02-27 11:04:20 +0530

    Add files via upload
    creating mongo db database in python and flask then inserting documents and deleting documents in the mongo db databases

--- flask_db/app.py
+++ flask_db/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from pymongo import MongoClient
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId # used to delete elements in DB
+
 
 # ...
 
@@ -18,4 +20,20 @@ client = MongoClient('localhost', 27017, username='root', password='1234')
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('index.html')
+    if request.method=='POST':
+        content = request.form['content']
+        degree = request.form['degree']
+        todos.insert_one({'content': content, 'degree': degree})
+        return redirect(url_for('index'))
+    all_todos = todos.find()
+    return render_template('index.html', todos=all_todos)
+#from pymongo import MongoClient
+
+
+# ...
+
+#deleting element in DB
+@app.post('/<id>/delete/')
+def delete(id):
+    todos.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('index'))
